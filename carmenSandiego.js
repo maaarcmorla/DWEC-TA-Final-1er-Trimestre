9a088ca9addfe82a839481419ff960684ca7c576
Marc Morlá Isern <marc.morla20@gmail.com>
2023-12-05 10:45:43 +0100

    Pequeños cambios en JS y muchos cambios en CSS

---
+++ carmenSandiego.js
@@ -0,0 +1,15 @@
+const ciutats = {
+    tokyo: "Tokyo",
+    berlin: "Berlín",
+    barcelona: "Barcelona",
+    moscow: "Moscow",
+    paris: "París"
+};
+
+const pistes = [
+    // falta poner pistas
+];
+
+function comencarJoc() {
+    document.getElementById("joc").style.display = "block";
+}
